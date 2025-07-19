12e684ddfbe011b604f9b2d30108f1377c5df03d
AbdElRahman-7 <abdelrahmanhamdy1999@gmail.com>
2025-07-19 18:30:36 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Banner from "./components/Banner";
+import Header from "./components/header/Header";
+
+export default function Home() {
+  return (
+      <>
+      <main>
+        <Banner />
+        
+       
+      </main>
+    </>
+  );
+}
